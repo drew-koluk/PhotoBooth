6375d6db9d7cd7b3729320dab40582be68f56262
Jakestoffyn <77401658+Jakestoffyn@users.noreply.github.com>
2025-07-02 21:22:43 -0400

    Update Camera.py

--- src/Camera.py
+++ src/Camera.py
@@ -1,22 +1,16 @@
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+cap = cv.VideoCapture(1)
+
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
- 
-    # if frame is read correctly ret is True
+
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Our operations on the frame come here
     
-    # Display the resulting frame
     cv.imshow('frame', frame)
-    if cv.waitKey(0):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
  
 # When everything done, release the capture
