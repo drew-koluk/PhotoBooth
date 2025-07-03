64901e28f5d87ba6ab0397fd39e2432252c4674c
drew-koluk <117960874+drew-koluk@users.noreply.github.com>
2025-07-02 21:24:50 -0400

    testing

--- src/Camera.py
+++ src/Camera.py
@@ -1,6 +1,6 @@
-import sys
+#import sys
 
-sys.path.append('/home/kali/.local/lib/python3.11/site-packages')
+#sys.path.append('C:\Users\drewk\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts')
 
 import cv2 as cv
 
